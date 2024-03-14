df8aae574277fd90b78381f2a706a85b839b2537
MarDevp <zarroukimariem01@gmail.com>
2024-03-14 22:19:29 +0100

    Create ReverseInterface.java

---
+++ exercice2/Client/ReverseInterface.java
@@ -0,0 +1,8 @@
+package Client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ReverseInterface extends Remote {
+    String reverseString(String input) throws RemoteException;
+}
